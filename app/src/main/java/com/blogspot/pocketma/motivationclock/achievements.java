e6e214d4743a6eb1076c6b99b2a24a9dd4868059
Batsa <Jayyang1994@gmail.com>
2017-10-20 20:06:17 -0700

    first commit

---
+++ app/src/main/java/com/blogspot/pocketma/motivationclock/achievements.java
@@ -0,0 +1,35 @@
+package com.blogspot.pocketma.motivationclock;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class achievements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievements);
+    }
+    public void openHome(View view)
+    {
+        Intent newIntent = new Intent(this, home.class);
+        startActivity(newIntent);
+    }
+    public void openAlarm(View view)
+    {
+        Intent newIntent = new Intent(this, alarm_change.class);
+        startActivity(newIntent);
+    }
+    public void openBored(View view)
+    {
+        Intent newIntent = new Intent(this, choose_task.class);
+        startActivity(newIntent);
+    }
+    public void openAchievements(View view)
+    {
+        Intent newIntent = new Intent(this, achievements.class);
+        startActivity(newIntent);
+    }
+}
